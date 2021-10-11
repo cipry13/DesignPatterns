bda24db51cb14cabcd6b28b288d95ffbe9f87eed
cipry13 <cipryy_13@yahoo.com>
2021-10-11 15:51:33 +0300

    Add Adapter

--- app/src/main/java/com/ch/designpatterns/structural/Adapter.kt
+++ app/src/main/java/com/ch/designpatterns/structural/Adapter.kt
@@ -1,1 +1,42 @@
 package com.ch.designpatterns.structural
+
+interface Android {
+    fun charge()
+}
+
+interface IPhone {
+    fun charge()
+}
+
+class AndroidCharger : Android {
+    override fun charge() {
+        println("Android phone is charging")
+    }
+}
+
+class IPhoneCharger : IPhone {
+    override fun charge() {
+        println("Iphone is charging")
+    }
+}
+
+class IPhoneToAndroidAdapter(private val androidPhone: Android) : IPhone {
+    override fun charge() {
+        androidPhone.charge()
+    }
+}
+
+fun main() {
+    // This will charge android phone
+    val androidCharger: Android = AndroidCharger()
+    androidCharger.charge()
+
+    // This will charge IPhone
+    val iPhoneCharger: IPhone = IPhoneCharger()
+    iPhoneCharger.charge()
+
+    // How to charge iphone with android charger?
+    val adapter = IPhoneToAndroidAdapter(androidCharger)
+    adapter.charge()
+}
+
